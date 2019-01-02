b4ab963706a757ec1a9c4f91dc028ab2183a8641
kosan0014 <mynamekosan@gmail.com>
2019-01-02 10:22:37 +0700

    LAB3 ex4,5,6

---
+++ src/LAB3/ex5.java
@@ -0,0 +1,18 @@
+package LAB3;
+
+import java.util.Scanner;
+
+public class ex5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = 1;
+
+        while (n != 0) {
+            System.out.println("Please enter numeber 0 to exit: ");
+            n = scanner.nextInt();
+        }
+
+        System.out.println("Bye.");
+
+    }//main
+}//class
---
+++ src/LAB3/ex6.java
@@ -0,0 +1,39 @@
+package LAB3;
+
+import java.util.Scanner;
+
+public class ex6 {
+
+    private String user = "admin"
+    private String pass = "1234"
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        String u=null,p=null;
+        boolean login = false;
+        int count = 0
+        do {
+            System.out.println("Enter username");
+            u = scanner.nextLine();
+            System.out.println("Entet passwoed");
+            p = scanner.nextLine();
+            if (u.equals(user) && p.equals(pass)){
+                login = true;
+            System.out.println("You had logged in");
+        }
+            else {
+                System.out.println("Inconrrect username or password");
+                login = false;
+                count++;
+                if (count ==3)
+                    System.out.println("You account have been logged");
+                    break;
+            }
+        }while (!login)
+
+
+
+    }//main
+}//class
+
