2e20e49fe2c3d733c44f74cc52f95cadc3715aa6
kosan0014 <mynamekosan@gmail.com>
2019-03-27 16:28:26 +0700

    jdbc

--- src/lab9_10/EmployeeApp.java
+++ src/lab9_10/EmployeeApp.java
@@ -1,9 +1,38 @@
 package lab9_10;
 
+import java.util.List;
+
 public class EmployeeApp {
     public static void main(String[] args) {
         empDAOImpl dao = empDAOImpl.getInstant();
 
-    }
+        //Get_ALL_emp
+        showData(dao);
+        //ADD_NEW_EMP(Create)
+        // dao.addEmp(new Employee(104,"kosan janpebun","gameing",150000));
+        //Find Employee
+        System.out.println("Find Employee with ID;");
+        Employee emp = dao.findEmp(350);
+        System.out.println(emp.toString());
 
+        //Update Employee
+        emp.setSalary(3000);
+        dao.updateEmp(emp);//update data in database
+        showData(dao);
+        //Dele Employee
+        dao.deleteEmp(350);
+        showData(dao);
+
+
+
+    }
+    private static void showData(empDAOImpl dao){
+
+        List<Employee> myemp = dao.getAllEmp();
+        System.out.println("Employee Data in Databass");
+
+        for (Employee emp : myemp) {
+            System.out.println(emp.toString());
+        }
+    }
 }
--- src/lab9_10/empDAOImpl.java
+++ src/lab9_10/empDAOImpl.java
@@ -1,6 +1,7 @@
 package lab9_10;
 
-import java.sql.Connection;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class empDAOImpl implements Employeedao {
@@ -9,9 +10,14 @@ public class empDAOImpl implements Employeedao {
     private static String url = "jdbc:sqlite:C:/Users/LabCom_4/company.sqlite";
     private static Connection conn = null;
 
-    //constant operetors
 
-    //constructor
+    //constant operation
+    private static final String GET_ALL_EMP = "select * from Employee";
+    private static final String ADD_new_Emp = "insert into employee" + "(id,name,position,salary) values (?,?,?,?)";
+    private static final String UPDATE_Emp = "update employee set name=?,position=?,salary=? where id=?";
+    private static final String DELETE_EMP = "delete from employee where id = ?";
+    private static final String FIND_BY_ID = "select * " + "from employee where id = ?";
+       //constructor
 
     private static empDAOImpl instant = new empDAOImpl();
     public static empDAOImpl getInstant() {
@@ -29,26 +35,119 @@ public class empDAOImpl implements Employeedao {
 
     @Override
     public List<Employee> getAllEmp() {
-        return null;
+        List<Employee>emp= new ArrayList<Employee>();
+        try {
+            conn=DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(GET_ALL_EMP);
+            //step4 : viewing data
+            while (rs.next()){
+                int id = rs.getInt(1);
+                String name = rs.getString(2);
+                String position = rs.getString(3);
+                double salary = rs.getDouble(4);
+                emp.add(new Employee(id, name, position, salary));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return emp;
     }
 
     @Override
     public void addEmp(Employee newEmp) {
+        try {
+            conn = DriverManager.getConnection(url);
+            PreparedStatement stmt = conn.prepareStatement(ADD_new_Emp);
+
+            stmt.setInt(1,newEmp.getId());
+            stmt.setString(2,newEmp.getName());
+            stmt.setString(3,newEmp.getPosition());
+            stmt.setDouble(4,newEmp.getSalary());
+
+            boolean ex = stmt.execute();
+            if (ex == false) System.out.println("Add new employee already");
+            else System.out.println("Could not add data");
+
+            stmt.close();
+            conn.close();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+
+        }
+
 
     }
 
     @Override
     public void updateEmp(Employee emp) {
+        try{
+            conn = DriverManager.getConnection(url);
+            PreparedStatement ps = conn.prepareStatement(UPDATE_Emp);
+
+            ps.setString(1,emp.getName());
+            ps.setString(2,emp.getPosition());
+            ps.setDouble(3,emp.getSalary());
+            ps.setInt(4,emp.getId());
+
+            int resultSet = ps.executeUpdate();
+            if (resultSet !=0)
+                System.out.println("Employee with id "+emp.getId()+"was updated following details:\n"+emp.toString());
+            else
+                System.out.println("Employee data was not updated.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
     @Override
     public void deleteEmp(int id) {
+        try{
+            conn = DriverManager.getConnection(url);
+            PreparedStatement stmt = conn.prepareStatement(DELETE_EMP);
+            stmt.setInt(1,id);
+            int resultSet = stmt.executeUpdate();
+            if (resultSet !=0){
+                System.out.println("Employee deled with id = "+id);
+                conn.close();
+            }
+            else {
+                System.out.println("No employee was dete wiyh id="+id);
+                conn.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 
     @Override
     public Employee findEmp(int id) {
-        return null;
+        Employee employee = null;
+        try{
+            conn = DriverManager.getConnection(url);
+            PreparedStatement ps = conn.prepareStatement(FIND_BY_ID);
+            ps.setInt(1,id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()){
+                int idd = rs.getInt(1);
+                String name = rs.getString(2);
+                String position = rs.getString(3);
+                double salary = rs.getDouble(4);
+
+                employee = new Employee(idd,name,position,salary);
+                }else {System.out.println("Employee with id ="+id+"not found.");}
+                    rs.close();
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return employee;
     }
 }
